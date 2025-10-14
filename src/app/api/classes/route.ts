49526e6c7dc5663aefe94dba542e2946b89fac7a
Compulink-Dev <devcompulink@gmail.com>
2025-10-14 16:58:19 +0200

    classes api

--- src/app/api/classes/route.ts
+++ src/app/api/classes/route.ts
@@ -3,6 +3,8 @@ import { NextResponse } from 'next/server';
 import prisma from '@/lib/prisma';
 import { auth } from '@clerk/nextjs/server';
 
+export const dynamic = 'force-dynamic';
+
 export async function GET() {
   try {
     const subjects = await prisma.subject.findMany({
